cf8cae929ceba499f4f0b30b277ac78e79746c98
Austin Joyal <anaustinbeing@gmail.com>
2021-10-28 20:25:49 -0400

    Changed db.uri to point to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://austin:T4ooy450QKJIYh35@mongodbserver.xsldw.mongodb.net/books229?retryWrites=true&w=majority"
 };
